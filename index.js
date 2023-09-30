e188af75a44734fa345e4c69d2e11818a3ef7871
Koovenn <educanaless@gmail.com>
2023-09-30 13:06:30 +0100

    Añadida_Solución

--- index.js
+++ index.js
@@ -1,71 +1,109 @@
 // VARIABLES
 // Crea una variable llamada "name" que contenga un string
+let name = "Eduardo";
 
 // Crea una variable llamada "age" que contenga un integer
+let age = 26;
 
 // Crea una variable llamada "hasHobbies" que contenga un booleano
+let hasHobbies = true; 
 
 // Crea una variable llamada "empty" que sea undefined
+let empty = undefined;
 
 // Crea una variable llamada "undefinedVariable" que sea undefined sin usar la palabra undefined
+let undefinedVariable; 
 
 // Crea una variable llamada "nullVariable" que sea null
+let nullVariable = null;
 
 // FUNCIONES
 // Crea una función llamada "sayHello" que retorne "Hello"
+function sayHello(){
+  return "Hello";
+}
 
 // Crea una función llamada "add" que reciba dos números como argumentos y retorne la suma de ambos
-
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
 
 // Crea una función llamada "sayHelloTo" que reciba un nombre como argumento y retorne "Hello, {name}"
+function sayHelloTo (name) {
+  return "Hello, " + name;
+}
 
 // CONDICIONALES
 // Crea una condición que retorne "true" si "firstParam" es mayor a "secondParam" y "false" si no lo es
-
 function isGreater(firstParam, secondParam) {
-  // Aquí va tu código
+  if (firstParam > secondParam) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crea una condición que retorne "true" si "firstParam" es igual a "secondParam" y "false" si no lo es
-
 function isEqual(firstParam, secondParam) {
-  // Aquí va tu código
+  if (firstParam = secondParam) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crea una condición que retorne "true" si "firstParam" es diferente a "secondParam" y "false" si no lo es
-
 function isDifferent(firstParam, secondParam) {
-  // Aquí va tu código
+  if (firstParam != secondParam) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crear una función llamada "isEven" que reciba un número como argumento y retorne "true" si es par o "false" si es impar
-
 function isEven(number) {
-  // Aquí va tu código
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crear una función llamada "isOdd" que reciba un número como argumento y retorne "true" si es impar o "false" si es par
-
 function isOdd(number) {
-  // Aquí va tu código
+  if (number % 2 == 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crear una función llamada "isPositive" que reciba un número como argumento y retorne "true" si es positivo o "false" si es negativo
-
 function isPositive(number) {
-  // Aquí va tu código
+  if (number >= 0 ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crea una condición que retorne "true" si "firstParam" es mayor a "secondParam" o "thirdParam" es mayor a "fourthParam" y "false" si no lo es
-
 function isGreaterOr(firstParam, secondParam, thirdParam, fourthParam) {
-  // Aquí va tu código
+  if (firstParam > secondParam || thirdParam > fourthParam) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crea una condición que retorne "true" si "firstParam" es mayor a "secondParam" y "thirdParam" es mayor a "fourthParam" y "false" si no lo es
-
 function isGreaterAnd(firstParam, secondParam, thirdParam, fourthParam) {
-  // Aquí va tu código
+  if (firstParam > secondParam && thirdParam > fourthParam) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Crea una función llamada "getRoleByName" que reciba un nombre como argumento
@@ -76,7 +114,14 @@ function isGreaterAnd(firstParam, secondParam, thirdParam, fourthParam) {
 // Si no es ninguna de esas personas debe retornar "Not found"
 
 // Esta parte se puede hacer con if/else o con switch/case
-
 function getAgeByName(name) {
-  // Aquí va tu código
+  if (name == "Giorgio") {
+    return "LI";
+  }else if (name == "Ivan") {
+      return "LI";
+  } else if (name == "Marcos") {
+    return "TA";
+  } else {
+    return "Not found";
+  }
 }
